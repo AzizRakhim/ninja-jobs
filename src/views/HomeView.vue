3fb765be9fad392c2813e922d457f17e9768768b
Aziz_Rakim <azizbekrahimjonov80@gmail.com>
2022-07-13 20:18:38 +0500

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo consequatur dignissimos quae praesentium reprehenderit nostrum expedita animi exercitationem numquam, ab sit dicta libero sunt nesciunt, earum perspiciatis beatae illum aperiam fugit cumque, dolores commodi aut. Voluptas, modi enim! Soluta, quisquam illo, tempora, aliquam veritatis quod id harum asperiores dolor eius earum vitae saepe rerum nemo consectetur culpa nostrum. Inventore alias ex atque sapiente, culpa, iusto eaque blanditiis eos earum aliquid architecto. Vitae rerum explicabo optio qui reiciendis vel expedita quaerat recusandae libero autem id fugiat quae, quam nemo exercitationem aut eos minus omnis repellat natus, maiores dicta amet! Vitae, quos.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo consequatur dignissimos quae praesentium reprehenderit nostrum expedita animi exercitationem numquam, ab sit dicta libero sunt nesciunt, earum perspiciatis beatae illum aperiam fugit cumque, dolores commodi aut. Voluptas, modi enim! Soluta, quisquam illo, tempora, aliquam veritatis quod id harum asperiores dolor eius earum vitae saepe rerum nemo consectetur culpa nostrum. Inventore alias ex atque sapiente, culpa, iusto eaque blanditiis eos earum aliquid architecto. Vitae rerum explicabo optio qui reiciendis vel expedita quaerat recusandae libero autem id fugiat quae, quam nemo exercitationem aut eos minus omnis repellat natus, maiores dicta amet! Vitae, quos.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
